c7f7b864374511a95be6174fb37ec889d508f65e
Glasss87 <44882277+Glasss87@users.noreply.github.com>
2019-08-26 02:18:58 -0300

    exit button

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -13,16 +13,19 @@ class Modal extends Component {
         return (
             <div className="modal">
                 <div className="box_modal">
+                    <button class="exit_button" onClick={() => this.props.handleUser('')}>
+                        <i class="fas fa-times"></i>
+                    </button>
                     <div className="top">
-                        <h1><i class="fas fa-map-marker-alt"></i>{this.props.user.nat}</h1>
-                        <img src={this.props.user.picture.large} />
-                        <h1>{this.props.user.dob.age}</h1>
+                        <h4><i class="fas fa-map-marker-alt"></i>{this.props.user.nat}</h4>
+                        <div className="photo"><img src={this.props.user.picture.large} /></div>
+                        <h4>{this.props.user.dob.age}</h4>
                     </div>
                     <div className="info">
                         <h1>{`${this.props.user.name.first} ${this.props.user.name.last}`}</h1>
                         <h2>@{this.props.user.login.username}</h2>
-                        <p>lorem</p>
-                        <p>{this.props.user.email}</p>
+                        <p>"Alias labore facere quidem vero odit doloremque molestias? Sint quae consequatur corrupti quisquam? Quod, eius!"</p>
+                        <p className="email">{this.props.user.email}</p>
                     </div>
                     <div className="footer">
                         <a href=""><i class="fab fa-linkedin-in"></i></a>
